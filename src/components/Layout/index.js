a1178120d43ab9402b64df989118c208ee0abc07
Matin Amani <matin26613@gmail.com>
2022-04-20 15:58:23 +0430

    drawer created

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,10 +4,13 @@ import Drawer from '@mui/material/Drawer'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import IconButton from '@mui/material/IconButton'
+import Divider from '@mui/material/Divider'
+
 import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded'
 import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded'
 
 import { useTheme } from '../../contexts/ThemeContext'
+import { DRAWER_WIDTH } from '../../constants'
 
 const Layout = ({ children }) => {
     const { themeMode, toggleTheme } = useTheme()
@@ -15,7 +18,29 @@ const Layout = ({ children }) => {
 
     return (
         <>
-            <AppBar color="primary" sx={{ px: 2 }} position="static">
+            <Drawer
+                sx={{
+                    width: DRAWER_WIDTH,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: DRAWER_WIDTH,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="permanent"
+            >
+                <Toolbar />
+                <Divider />
+            </Drawer>
+            <AppBar
+                color="primary"
+                sx={{
+                    px: 2,
+                    width: `calc(100% - ${DRAWER_WIDTH}px)`,
+                    ml: `${DRAWER_WIDTH}px`,
+                }}
+                position="static"
+            >
                 <Toolbar>
                     <Typography sx={{ flexGrow: 1 }} variant="h4">
                         Admin Panel
@@ -32,8 +57,14 @@ const Layout = ({ children }) => {
                     </IconButton>
                 </Toolbar>
             </AppBar>
-
-            {children}
+            <Box
+                sx={{
+                    width: `calc(100% - ${DRAWER_WIDTH}px)`,
+                    ml: `${DRAWER_WIDTH}px`,
+                }}
+            >
+                {children}
+            </Box>
         </>
     )
 }
