a13523166e137418d5eacce63f3c786d6e51954a
Matin Amani <matin26613@gmail.com>
2022-05-01 18:38:07 +0430

    actions column added to users datagrid

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,7 +1,11 @@
 import { useState, useEffect } from 'react'
 import { DataGrid } from '@mui/x-data-grid'
 import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
 import { useNavigate } from 'react-router-dom'
+
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/Delete'
 
 import axios from '../helpers/api'
 import { COLORS } from '../constants'
@@ -10,6 +14,8 @@ import { CircularProgress } from '@mui/material'
 const Users = () => {
     const [users, setUsers] = useState([])
     const [loading, setLoading] = useState(false)
+    const [pageSize, setPageSize] = useState(9)
+    const navigate = useNavigate()
 
     const columns = [
         { field: 'id', headerName: 'ID', width: 90 },
@@ -24,6 +30,7 @@ const Users = () => {
                         }}
                         src={value.src}
                         alt={value.alt}
+                        onClick={() => navigate(`/users/${value.id}`)}
                     >
                         {value.children}
                     </Avatar>
@@ -41,6 +48,35 @@ const Users = () => {
             field: 'email',
             headerName: 'Email',
             width: 250,
+        },
+        {
+            field: 'edit',
+            headerName: 'Edit',
+            renderCell: ({ value }) => {
+                return (
+                    <IconButton
+                        color="primary"
+                        onClick={() => navigate(`/users/${value}/edit`)}
+                    >
+                        <EditIcon />
+                    </IconButton>
+                )
+            },
+            width: 90,
+            sortable: false,
+        },
+        {
+            field: 'delete',
+            headerName: 'Delete',
+            renderCell: ({ value }) => {
+                return (
+                    <IconButton color="error">
+                        <DeleteIcon />
+                    </IconButton>
+                )
+            },
+            width: 90,
+            sortable: false,
         },
     ]
 
@@ -60,6 +96,7 @@ const Users = () => {
                 ...res1.data.map((user) => ({
                     id: user.id,
                     avatar: {
+                        id: user.id,
                         src: user.avatar,
                         alt: user.email,
                         color: pickRandomColor(),
@@ -67,6 +104,8 @@ const Users = () => {
                     },
                     email: user.email,
                     name: `${user.first_name} ${user.last_name}`,
+                    edit: user.id,
+                    delete: user.id,
                 })),
                 ...res2.data.map((user) => ({
                     id: user.id,
@@ -78,6 +117,8 @@ const Users = () => {
                     },
                     email: user.email,
                     name: `${user.first_name} ${user.last_name}`,
+                    edit: user.id,
+                    delete: user.id,
                 })),
             ])
         } catch (err) {
@@ -91,17 +132,16 @@ const Users = () => {
         getUsers()
     }, [])
 
-    const navigate = useNavigate()
-
     return loading ? (
         <CircularProgress />
     ) : (
         <DataGrid
             rows={users}
             columns={columns}
-            pageSize={6}
-            rowsPerPageOptions={[6]}
-            onCellClick={({ row: { id } }) => navigate(`/users/${id}`)}
+            pageSize={pageSize}
+            rowsPerPageOptions={[6, 9, 12]}
+            disableColumnMenu
+            onPageSizeChange={(size) => setPageSize(size)}
         />
     )
 }
